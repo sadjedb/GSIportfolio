61546fdaecd92fbb30b14da455ff43a768869914
sadjedb <sadjedbougandoura@gmail.com>
2024-10-11 17:34:18 +0100

    fixing the build thing and changing the photos and the languageee

--- components/Language.jsx
+++ components/Language.jsx
@@ -27,11 +27,11 @@ const LanguageSelector = () => {
       <div>
         <button
           type="button"
-          className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-md font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+          className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4  py-2 bg-white text-md font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
           onClick={toggleDropdown}
         >
           <span className="mr-2">{selectedLanguage.flag}</span>
-          {selectedLanguage.name}
+          <span className="hidden md:flex">{selectedLanguage.name}</span>
           <ChevronDown className="ml-2 h-5 w-5" />
         </button>
       </div>
