fc316ea1c3232f33cbe23fd2926d5e729a4eaa69
sadjedb <sadjedbougandoura@gmail.com>
2024-10-11 11:15:09 +0100

    fixing the build thing and changing the photos and the languagess

--- components/NavItems.jsx
+++ components/NavItems.jsx
@@ -8,37 +8,46 @@ const NavItems = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="" onClick={() => setOpen((prev) => !prev)}>
-      <Menu size={24} color="#000000" />
+    <div className="relative z-50">
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className="z-50 relative"
+      >
+        <Menu size={24} color="#000000" />
+      </button>
       {open && (
-        <div className="absolute bg-white text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-3xl  z-10">
+        <div className="fixed inset-0 bg-white flex flex-col items-center justify-center gap-8 text-3xl z-40">
           <Link
             href="/"
-            className="group text-gray-700 transition duration-500 hover:text-[#10183C] "
+            className="group text-gray-700 transition duration-500 hover:text-[#10183C]"
+            onClick={() => setOpen(false)}
           >
             Home
-            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#10183C]"></span>{" "}
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#10183C]"></span>
           </Link>
           <Link
             href="/aboutus"
-            className="group text-gray-700 transition duration-500 hover:text-[#10183C] "
+            className="group text-gray-700 transition duration-500 hover:text-[#10183C]"
+            onClick={() => setOpen(false)}
           >
             About Us
             <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#10183C]"></span>
           </Link>
           <Link
             href="/contact"
-            className="group text-gray-700 transition duration-500 hover:text-[#10183C] "
+            className="group text-gray-700 transition duration-500 hover:text-[#10183C]"
+            onClick={() => setOpen(false)}
           >
             Contact
-            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#10183C]"></span>{" "}
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#10183C]"></span>
           </Link>
           <Link
             href="/catalog"
-            className="group text-gray-700 transition duration-500 hover:text-[#10183C] "
+            className="group text-gray-700 transition duration-500 hover:text-[#10183C]"
+            onClick={() => setOpen(false)}
           >
             Catalog
-            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#10183C]"></span>{" "}
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#10183C]"></span>
           </Link>
         </div>
       )}
